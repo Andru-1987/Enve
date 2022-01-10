cb9d712dd08eb0ace95c57f8435ebacf1fc5929a
Anderson Ocaña <86977326+Andru-1987@users.noreply.github.com>
2022-01-10 14:27:29 -0300

    Update Primeros.js

--- danielEjerciciosJS/Primeros.js
+++ danielEjerciciosJS/Primeros.js
@@ -1,30 +1,32 @@
 // Ejercicio de prompt y alert
 
 
-function pedirMostrarData(){
+(function pedirMostrarData(){
 
  	let array=[];
-
+  let data;
+  
 	for (let i=0; i<3;i++){
     if(i!=2){
-      let data=Number(prompt(`insert data ${i}: `));
+      data=Number(prompt(`insert data ${i}: `));
     } else{
-      let data=prompt(`insert data ${i}: `);
+      data=prompt(`insert data ${i}: `);
     }
 		array.push(data);
 	};
 
-    let arrayNovo=[];
+  let arrayNovo=[];
   for(let i=0;i<2;i++){
-    let data=Number(prompt('insert array data numbers'));
+     data=Number(prompt('insert array data numbers'));
     arrayNovo.push(data);
   }
 
   array.push(arrayNovo);
+  
 	localStorage.setItem('Ingresado', array );
 	array.forEach((c,index) => alert (`El valor ${index} es: ${c}`));
 
-};
+})();
 
 
 
